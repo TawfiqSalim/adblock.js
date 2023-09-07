3484f18383e7238079d36c79b8ae23315a8a75cc
TawfiqSalim <85137519+TawfiqSalim@users.noreply.github.com>
2023-09-07 12:00:11 +0200

    Update and rename alert-not.html to alert-not.php

--- alert-not.php
+++ alert-not.php
@@ -1,7 +1,7 @@
 <div class="notice notice-info is-dismissible" id="my-plugin-review-notice">
         <p><?php _e('Enjoying My Plugin? Please take a moment to leave a review!', 'wprw'); ?></p>
         <p>
-            <a class="button button-primary" href="https://example.com/review-page"><?php _e('Leave a Review', 'my-plugin-textdomain'); ?></a>
+            <a class="button button-primary" href="https://example.com/review-page"><?php _e('Leave a Review', 'wprw'); ?></a>
             <button class="button" id="my-plugin-review-dismiss"><?php _e('I already made a review', 'wprw'); ?></button>
         </p>
     </div>
